97f847a8df897db20d176d7bf1d648f4e8c39af5
VCube1003 <140152735+VCube1003@users.noreply.github.com>
2024-06-14 19:46:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var balance = 10
+var balance = 10000
 var corn_price = 1
 var land_cost = 10
 var land_power = 1
@@ -7,7 +7,8 @@ var cps = 0
 var land = 0
 var marketing_cost = 50
 var dps = 0
-var marketing = 1
+var marketing = 0
+var pp = 0
 
 setInterval(GUIupdate, 50)
 setInterval(updateBalance, 1000)
@@ -21,10 +22,35 @@ function GUIupdate(){
   document.getElementById("cps").innerHTML = "Corn/sec: " + cps
   document.getElementById("dpc").innerHTML = "Corn price: $" + corn_price
   document.getElementById("dpc_upgrade").innerHTML = "Marketing: Increase demand to increase the price of corn by $0.25 (Cost: $" + marketing_cost + ")"
+  document.getElementById("prestige").innerHTML = "Prestige for " + Math.floor(Math.sqrt(balance/10000)) + " prestige points"
+  document.getElementById("next_prestige").innerHTML = "Next prestige point at $" + ((Math.floor(Math.sqrt(balance/10000))+1)**2*10000)
+  document.getElementById("ppcount").innerHTML = "Prestige Points: " + pp
 }
 
 function updateBalance(){
   balance += dps
+  if (balance >= 10000){
+    prestige.removeAttribute("hidden")
+    next_prestige.removeAttribute("hidden")
+    ppcount.removeAttribute("hidden")
+    puclass.removeAttribute("hidden")
+  }
+}
+
+function prestige_function(){
+  if (balance >= 10000){
+    pp += Math.floor(Math.sqrt(balance/10000))
+    balance = 10
+    corn_price = 1
+    land_cost = 10
+    land_power = 1
+    marketing_power = 0.25
+    cps = 0
+    land = 0
+    marketing_cost = 50
+    dps = 0 
+    marketing = 1
+  }
 }
 
 function buyLand(){
