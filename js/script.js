6ef2e9dfad2644b9c3b1edf3527c4847c885b10e
VCube1003 <140152735+VCube1003@users.noreply.github.com>
2024-06-16 11:29:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,34 +2,48 @@ var balance = 10
 var corn_price = 1
 var land_cost = 10
 var land_power = 1
-var marketing_power = 0.25
 var cps = 0
 var land = 0
 var marketing_cost = 50
 var dps = 0
 var marketing = 0
 var pp = 0
+var u1_amt = [0, 10000, 1]
+var u2_amt = [0, 50000, 0.25]
 
 setInterval(GUIupdate, 50)
 setInterval(updateBalance, 1000)
+setInterval(statsUpdate, 50)
 
 function GUIupdate(){
-  document.getElementById("balance").innerHTML = "Balance: $" + balance
-  document.getElementById("dps").innerHTML = "$/sec: " + dps
-  document.getElementById("land").innerHTML = "Land: " + land + " (Cost: $" + land_cost + ")"
-  document.getElementById("land_power").innerHTML = "Each land produces " + land_power + " corn per second"
-  document.getElementById("multiplier").innerHTML = "Multiplier: " + land_power
-  document.getElementById("cps").innerHTML = "Corn/sec: " + cps
-  document.getElementById("dpc").innerHTML = "Corn price: $" + corn_price
-  document.getElementById("dpc_upgrade").innerHTML = "Marketing: Increase demand to increase the price of corn by $0.25 (Cost: $" + marketing_cost + ")"
-  document.getElementById("prestige").innerHTML = "Prestige for " + Math.floor(Math.sqrt(balance/10000)) + " prestige points"
-  document.getElementById("next_prestige").innerHTML = "Next prestige point at $" + ((Math.floor(Math.sqrt(balance/10000))+1)**2*10000)
-  document.getElementById("ppcount").innerHTML = "Prestige Points: " + pp
+  document.getElementById("balance").textContent = "Balance: $" + Math.round(balance*100)/100
+  document.getElementById("dps").textContent = "$/sec: " + Math.round(dps*100)/100
+  document.getElementById("land").textContent = "Land: " + land + " (Cost: $" + land_cost + ")"
+  document.getElementById("land_power").textContent = "Each land produces " + Math.round(land_power*u1_amt[2]*100)/100 + " corn per second"
+  document.getElementById("multiplier").textContent = "Multiplier: " + land_power
+  document.getElementById("cps").textContent = "Corn/sec: " + Math.round(cps*100)/100
+  document.getElementById("dpc").textContent = "Corn price: $" + Math.round(corn_price*100)/100
+  document.getElementById("dpc_upgrade").textContent = "Marketing: Increase demand to increase the price of corn by $" + Math.round(100*u2_amt[2])/100 + " (Cost: $" + marketing_cost + ")"
+  document.getElementById("prestige").textContent = "Prestige for " + Math.floor(Math.sqrt(balance/1000000)) + " prestige points"
+  document.getElementById("next_prestige").textContent = "Next prestige point at $" + ((Math.floor(Math.sqrt(balance/1000000))+1)**2*1000000)
+  document.getElementById("ppcount").textContent = "Prestige Points: " + pp
+  document.getElementById("u1_text").textContent = "Genetic Modifications: Increases crop yield (" + Math.round(u1_amt[2]*100)/100 + "x → " + Math.round(100*(u1_amt[2] + 0.7))/100 + "x)"
+  document.getElementById("buy_u1").textContent = "Buy upgrade (" + Math.round(u1_amt[0]*100)/100 + "/5)"
+  document.getElementById("u1_cost").textContent = "Cost: $" + Math.round(u1_amt[1]*100)/100
+  document.getElementById("u2_text").textContent = "Better Marketing: Increased corn price gain per marketing ($" + Math.round(u2_amt[2]*100)/100 + " → $" + Math.round(100*(u2_amt[2] + 0.15))/100 + ")"
+  document.getElementById("buy_u2").textContent = "Buy upgrade (" + Math.round(u2_amt[0]*100)/100 + "/5)"
+  document.getElementById("u2_cost").textContent = "Cost: $" + Math.round(u2_amt[1]*100)/100
+}
+
+function statsUpdate(){
+  cps = land*land_power*u1_amt[2]
+  dps = cps*corn_price
+  corn_price = 1+u2_amt[2]*marketing
 }
 
 function updateBalance(){
   balance += dps
-  if (balance >= 10000){
+  if (balance >= 1000000){
     prestige.removeAttribute("hidden")
     next_prestige.removeAttribute("hidden")
     ppcount.removeAttribute("hidden")
@@ -38,18 +52,19 @@ function updateBalance(){
 }
 
 function prestige_function(){
-  if (balance >= 10000){
-    pp += Math.floor(Math.sqrt(balance/10000))
+  if (balance >= 1000000){
+    pp += Math.floor(Math.sqrt(balance/1000000))
     balance = 10
     corn_price = 1
     land_cost = 10
     land_power = 1
-    marketing_power = 0.25
     cps = 0
     land = 0
     marketing_cost = 50
     dps = 0 
     marketing = 1
+    u1_amt = [0, 10000, 1]
+    u2_amt = [0, 50000, 0.25]
   }
 }
 
@@ -61,8 +76,6 @@ function buyLand(){
     balance -= land_cost
     land += 1
     land_cost = Math.floor(land_cost*1.4)
-    cps = land*land_power
-    dps = cps*corn_price
   }
 }
 
@@ -71,7 +84,22 @@ function buyMarketing(){
     balance -= marketing_cost
     marketing += 1
     marketing_cost = Math.floor(marketing_cost*1.4) 
-    corn_price = 1+marketing_power*marketing
-    dps = cps*corn_price
+  }
+}
+
+function u1(){
+  if ((balance >= u1_amt[1]) && (u1_amt[0] < 5)){
+    balance -= u1_amt[1]
+    u1_amt[0] += 1
+    u1_amt[1] = Math.round(((u1_amt[0]+1)**2.5)*10000)
+    u1_amt[2] += 0.7
+  }
+}
+function u2(){
+  if ((balance >= u2_amt[1]) && (u2_amt[0] < 5)){
+    balance -= u2_amt[1]
+    u2_amt[0] += 1
+    u2_amt[1] = Math.round(((u2_amt[0]+1)**2)*50000)
+    u2_amt[2] += 0.15
   }
 }
